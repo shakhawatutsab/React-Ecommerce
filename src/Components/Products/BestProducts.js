ecd32c1d7c8faa5f08ef8232f35eb15372583e07
utsab <shakhawatutsab@gmail.com>
2025-12-11 00:26:25 +0600

    React ecommerce single product components

---
+++ src/Components/Products/BestProducts.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import SingleProductCard from './SingleProductCard';
+const BestProducts = () => {
+  return (
+    <>
+      <section className="products-carousel container">
+        <h2
+          className="section-title text-center fw-normal text-uppercase mb-1 mb-md-3 pb-xl-3"
+        >
+          Best Selling Products
+        </h2>
+
+        <ul
+          className="nav nav-tabs mb-3 pb-3 mb-xl-4 text-uppercase justify-content-center"
+          id="collections-tab"
+          role="tablist"
+        >
+          <li className="nav-item" role="presentation">
+            <a
+              className="nav-link nav-link_underscore active"
+              id="collections-tab-1-trigger"
+              data-bs-toggle="tab"
+              href="#collections-tab-1"
+              role="tab"
+              aria-controls="collections-tab-1"
+              aria-selected="true"
+              >All</a
+            >
+          </li>
+          <li className="nav-item" role="presentation">
+            <a
+              className="nav-link nav-link_underscore"
+              id="collections-tab-2-trigger"
+              data-bs-toggle="tab"
+              href="#collections-tab-2"
+              role="tab"
+              aria-controls="collections-tab-2"
+              aria-selected="true"
+              >Featured</a
+            >
+          </li>
+          <li className="nav-item" role="presentation">
+            <a
+              className="nav-link nav-link_underscore"
+              id="collections-tab-3-trigger"
+              data-bs-toggle="tab"
+              href="#collections-tab-3"
+              role="tab"
+              aria-controls="collections-tab-3"
+              aria-selected="true"
+              >Best Seller</a
+            >
+          </li>
+          <li className="nav-item" role="presentation">
+            <a
+              className="nav-link nav-link_underscore"
+              id="collections-tab-4-trigger"
+              data-bs-toggle="tab"
+              href="#collections-tab-4"
+              role="tab"
+              aria-controls="collections-tab-4"
+              aria-selected="true"
+              >Sales</a
+            >
+          </li>
+        </ul>
+
+        <div className="tab-content pt-2" id="collections-tab-content">
+          <div
+            className="tab-pane fade show active"
+            id="collections-tab-1"
+            role="tabpanel"
+            aria-labelledby="collections-tab-1-trigger"
+          >
+            <div className="row">
+                {/* Single Product Starts here */}
+                <SingleProductCard />
+                {/* Single Product Ends here */}
+              
+            </div>
+            {/* /.row */}
+            <div className="text-center mt-2">
+              <a
+                className="btn-link btn-link_lg default-underline text-uppercase fw-medium"
+                href="shop1.html"
+                >See All Products</a
+              >
+            </div>
+          </div>
+          
+        </div>
+        {/* /.tab-content pt-2 */}
+      </section>
+      {/* /.products-grid */}
+    </>
+  )
+}
+
+export default BestProducts;
---
+++ src/Components/Products/SingleProductCard.js
@@ -0,0 +1,123 @@
+import React from 'react'
+
+const SingleProductCard = () => {
+  return (
+    <>
+      <div className="col-6 col-md-4 col-lg-3">
+            <div className="product-card mb-3 mb-md-4 mb-xxl-5">
+                <div className="pc__img-wrapper">
+                <div
+                    className="swiper-container background-img js-swiper-slider"
+                    data-settings='{"resizeObserver": true}'
+                >
+                    <div className="swiper-wrapper">
+                    <div className="swiper-slide">
+                        <a href="product1_simple.html">
+                        <img
+                            loading="lazy"
+                            src="https://uomo-html.flexkitux.com/images/home/demo9/product-1-1.jpg"
+                            width="330"
+                            height="400"
+                            alt="Colorful Jacket"
+                            className="pc__img"
+                        />
+                        </a>
+                    </div>
+                    {/* /.pc__img-wrapper */}
+                    <div className="swiper-slide">
+                        <a href="product1_simple.html">
+                        <img
+                            loading="lazy"
+                            src="https://uomo-html.flexkitux.com/images/home/demo9/product-1-1.jpg"
+                            width="330"
+                            height="400"
+                            alt="Colorful Jacket"
+                            className="pc__img"
+                        />
+                        </a>
+                    </div>
+                    {/* /.pc__img-wrapper */}
+                    <div className="swiper-slide">
+                        <a href="product1_simple.html">
+                        <img
+                            loading="lazy"
+                            src="https://uomo-html.flexkitux.com/images/home/demo9/product-1-1.jpg"
+                            width="330"
+                            height="400"
+                            alt="Colorful Jacket"
+                            className="pc__img"
+                        />
+                        </a>
+                    </div>
+                    {/* /.pc__img-wrapper */}
+                    </div>
+                    <span className="pc__img-prev"
+                    ><svg
+                        width="7"
+                        height="11"
+                        viewBox="0 0 7 11"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <use href="#icon_prev_sm" /></svg
+                    ></span>
+                    <span className="pc__img-next"
+                    ><svg
+                        width="7"
+                        height="11"
+                        viewBox="0 0 7 11"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <use href="#icon_next_sm" /></svg
+                    ></span>
+                </div>
+                <button
+                    className="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium js-add-cart js-open-aside"
+                    data-aside="cartDrawer"
+                    title="Add To Cart"
+                >
+                    <svg
+                    className="d-inline-blockk align-middle mx-2"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <use href="#icon_cart" /></svg
+                    ><span className="d-inline-block align-middle"
+                    >Add To Cart</span
+                    >
+                </button>
+                </div>
+
+                <div className="pc__info position-relative">
+                <p className="pc__category">Accessories</p>
+                <h6 className="pc__title mb-2">
+                    <a href="product1_simple.html">Wooden Wall Clock</a>
+                </h6>
+                <div className="product-card__price d-flex">
+                    <span className="money price">$29</span>
+                </div>
+
+                <button
+                    className="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist"
+                    title="Add To Wishlist"
+                >
+                    <svg
+                    width="16"
+                    height="16"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    >
+                    <use href="#icon_heart" />
+                    </svg>
+                </button>
+                </div>
+            </div>
+            </div>
+    </>
+  )
+}
+
+export default SingleProductCard
