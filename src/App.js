a964fbdd86a78f41415dbae8166594100ad6405d
utsab <shakhawatutsab@gmail.com>
2026-01-03 15:12:13 +0600

    Home Page Routing

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
-import CartDrawer from './Components/CartDrawer';
-import Footer from './Components/Footer';
-import FooterMobile from './Components/FooterMobile';
-import Header from './Components/Header';
-import LoginForm from './Components/LoginForm';
-import Main from './Components/Main';
-import NewsLetter from './Components/NewsLetter';
-import PageOverlay from './Components/PageOverlay';
-import QuickView from './Components/QuickView';
-import ScrollToTop from './Components/ScrollToTop';
-import SiteMap from './Components/SiteMap';
-
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
 function App() {
   return (
-    <>
-      <Header />
-
-      <Main />
-
-    {/* Footer Type 2 */}
-      <Footer />
-
-    {/* Mobile Fixed Footer */}
-    <FooterMobile />
-    {/* /.footer-mobile container position-fixed d-md-none bottom-0 */}
-
-    {/* Customer Login Form */}
-    <LoginForm />
-    {/* /.aside aside_right */}
-
-    {/* Cart Drawer */}
-    <CartDrawer />
-    {/* /.aside */}
-
-    {/* Sitemap */}
-    <SiteMap />
-    {/* /.sitemap position-fixed w-100 */}
-
-    {/* Quick View */}
-    <QuickView />
-    {/* /.quickview position-fixed */}
-
-    {/* Newsletter Popup */}
-    <NewsLetter />
-    {/* /.newsletter-popup position-fixed */}
-
-    {/* Go To Top */}
-    <ScrollToTop  />
-
-    {/* Page Overlay */}
-    <PageOverlay />
-    {/* /.page-overlay */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Header from '../Components/Header';
+import Main from '../Components/Main';
+import Footer from '../Components/Footer';
+import FooterMobile from '../Components/FooterMobile';
+import LoginForm from '../Components/LoginForm';
+import CartDrawer from '../Components/CartDrawer';
+import SiteMap from '../Components/SiteMap';
+import QuickView from '../Components/QuickView';
+import NewsLetter from '../Components/NewsLetter';
+import ScrollToTop from '../Components/ScrollToTop';
+import PageOverlay from '../Components/PageOverlay';
+const Homepage = () => {
+  return (
+    <>
+    <Header />
+
+      <Main />
+    {/* Footer Type 2 */}
+      <Footer />
+
+    {/* Mobile Fixed Footer */}
+    <FooterMobile />
+    {/* /.footer-mobile container position-fixed d-md-none bottom-0 */}
+
+    {/* Customer Login Form */}
+    <LoginForm />
+    {/* /.aside aside_right */}
+
+    {/* Cart Drawer */}
+    <CartDrawer />
+    {/* /.aside */}
+
+    {/* Sitemap */}
+    <SiteMap />
+    {/* /.sitemap position-fixed w-100 */}
+
+    {/* Quick View */}
+    <QuickView />
+    {/* /.quickview position-fixed */}
+
+    {/* Newsletter Popup */}
+    <NewsLetter />
+    {/* /.newsletter-popup position-fixed */}
+
+    {/* Go To Top */}
+    <ScrollToTop  />
+
+    {/* Page Overlay */}
+    <PageOverlay />
+    {/* /.page-overlay */}
+    </>
+  )
+}
+
+export default Homepage;
+
