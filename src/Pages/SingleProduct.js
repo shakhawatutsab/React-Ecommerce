79bb9c3219204028ea6734dc6ee886731c06bd98
utsab <shakhawatutsab@gmail.com>
2026-01-04 21:38:39 +0600

    SingleProduct get id use useParams()

--- src/Pages/SingleProduct.js
+++ src/Pages/SingleProduct.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const SingleProduct = () => {
+    const { id } = useParams();
   return (
-    <div>SingleProduct</div>
+    <div>SingleProduct : {id}</div>
   )
 }
 
