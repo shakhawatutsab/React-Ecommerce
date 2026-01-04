b22f63494759e3ef6600fd4c8322c2a03494098d
utsab <shakhawatutsab@gmail.com>
2026-01-04 21:47:13 +0600

    Fix css file for SingleProduct page

--- src/Pages/SingleProduct.js
+++ src/Pages/SingleProduct.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import Header from '../Components/Header';
+import Main from '../Components/Main';
+import Footer from '../Components/Footer';
+import FooterMobile from '../Components/FooterMobile';
+import LoginForm from '../Components/LoginForm';
+import CartDrawer from '../Components/CartDrawer';
+import SiteMap from '../Components/SiteMap';
+import QuickView from '../Components/QuickView';
+import NewsLetter from '../Components/NewsLetter';
+import ScrollToTop from '../Components/ScrollToTop';
+import PageOverlay from '../Components/PageOverlay';
 
 const SingleProduct = () => {
     const { id } = useParams();
   return (
-    <div>SingleProduct : {id}</div>
+    <>
+    <Header />
+    </>
   )
 }
 
-export default SingleProduct
+export default SingleProduct;
